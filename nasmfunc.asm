09bd3d9ed6aa97f48fd0efb74c1eca16edc1ee30
zeroclock <zeroclock@icloud.com>
2020-03-17 07:22:15 +0900

    04: show white screen

--- nasmfunc.asm
+++ nasmfunc.asm
@@ -1,7 +1,14 @@
 bits 32
 global io_hlt
+global write_mem8
 
 section .text
 io_hlt:
         HLT
+        RET
+
+write_mem8:     ; void write_mem8(int addr, int data);
+        MOV     ECX,[ESP+4]     ; addr
+        MOV     AL,[ESP+8]      ; data
+        MOV     [ECX],AL
         RET
